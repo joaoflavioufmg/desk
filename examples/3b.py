72e42951b0902894dd6131b2764d4e56e91814e1
joaoflavioufmg <joao.flavio@dep.ufmg.br>
2025-10-27 20:22:32 -0300

    3b.py Troncos aux

--- examples/3b.py
+++ examples/3b.py
@@ -472,7 +472,7 @@ def main():
 
 
 if __name__ == "__main__":
-    model, logger = main()
+    # model, logger = main()
     # run_replications()
     # factorial = ex3_factorial_analysis()
-    # run_visualization(build_ex3_model, simulation_time=60)
+    run_visualization(build_ex3_model, simulation_time=60)
